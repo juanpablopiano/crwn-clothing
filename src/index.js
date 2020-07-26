bd124ba8bb42f38346979331bf2a0fc9cff026ac
Juan Gonzalez <jgu1@hotmail.com>
2020-07-26 13:28:03 -0400

    Add cartIcon and cartDropdown components and implement cart reducer

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
-import store from "./redux/store"
+import store from "./redux/store";
 
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter>
+			<React.StrictMode>
 				<App />
-			</BrowserRouter>
-		</Provider>
-	</React.StrictMode>,
+			</React.StrictMode>
+		</BrowserRouter>
+	</Provider>,
 	document.getElementById("root")
 );
