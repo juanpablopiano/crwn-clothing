69cfbc07da0bab8b9e0973d7dc22655c83b2310a
Juan Gonzalez <jgu1@hotmail.com>
2020-08-05 22:57:54 -0400

    Change header, homepage and custombutton components to use styled-components library

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import Directory from '../../components/directory/directoy.component';
+import Directory from "../../components/directory/directoy.component";
 
-import './homepage.styles.scss';
+import { HomePageContainer } from "./homepage.styles";
 
 const HomePage = () => (
-	<div className="homepage">
+	<HomePageContainer>
 		<Directory />
-	</div>
-)
+	</HomePageContainer>
+);
 
 export default HomePage;
